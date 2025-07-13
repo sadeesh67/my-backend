44e5a92adcb4acf34bd303f357e2a29765636f0a
sadeesh67 <sadeesh671999@gmail.com>
2025-07-13 08:53:46 +0530

    Fix react-scripts permissions and dependencies

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://your-netlify-site.netlify.app"],
+  credentials: true
+}));
 app.use(express.json());
 
 
